eedb58136b43ad8281dbe5b7f76626d9228cacf9
dunson-orange <mpdunson@gmail.com>
2022-07-10 13:12:46 -0400

    added specialities, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author mattdunson
+ * @Date 7/10/22
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,8 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by m.d. on 08/08/2021
  */
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
