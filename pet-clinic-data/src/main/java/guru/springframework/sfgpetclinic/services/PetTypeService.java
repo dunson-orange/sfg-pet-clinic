e856773e07ca776ceb5a1419c5997e4aa44dca0c
dunson-orange <mpdunson@gmail.com>
2022-07-10 13:29:15 -0400

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author mattdunson
+ * @Date 7/10/22
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
